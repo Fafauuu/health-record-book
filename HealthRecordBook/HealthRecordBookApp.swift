48db32bb0ec5d510a2b9fa30a43fd21a27242e73
rafalkwiecien <rafalkw00@gmail.com>
2023-11-01 10:50:48 +0100

    Initial Commit

---
+++ HealthRecordBook/HealthRecordBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthRecordBookApp.swift
+//  HealthRecordBook
+//
+//  Created by Rafał Kwiecień on 01/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct HealthRecordBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
